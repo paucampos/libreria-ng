4f524ea28f05c35881144c97e70b0c5fd9ceed8c
PauCampos <pau.campos89@gmail.com>
2021-03-20 16:19:10 -0300

    creacion de libreria cards

---
+++ projects/cards/src/lib/cards.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tng-cards',
+  template: `
+    <p>
+      cards works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/cards/src/lib/cards.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CardsComponent } from './cards.component';
+
+
+
+@NgModule({
+  declarations: [CardsComponent],
+  imports: [
+  ],
+  exports: [CardsComponent]
+})
+export class CardsModule { }
